91bbee0b217fddeaea7f6db524b018b3ff456706
Carlos Segoviano <jcsegoviano@planeta.com.mx>
2024-04-18 18:55:59 +0200

    Se crean archivos de test para JCAP

---
+++ testdev/JCAP/querys-mysql-test.php
@@ -0,0 +1,20 @@
+<?php
+include("../conexion/conexion.php");
+
+
+// echo "<pre>";
+// print_r($_SESSION);
+// echo "</pre>";
+
+$sql 	= "SELECT * FROM users LIMIT 10";
+$query 	= db_select($sql);
+
+echo "<pre>";
+
+foreach ($query as $key => $value) {
+ 	echo $value['username'].'<br>'.$value['nivel'];
+}
+
+echo "</pre>";
+
+?>
