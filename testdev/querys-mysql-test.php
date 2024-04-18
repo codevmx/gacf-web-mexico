f1ce442aebacedc944e4760ac8237b7b933d7a9f
Carlos Segoviano <jcsegoviano@planeta.com.mx>
2024-04-18 06:11:44 +0200

    Se agregan archivos de test querys

---
+++ testdev/querys-mysql-test.php
@@ -0,0 +1,46 @@
+<?php
+include("../conexion/conexion.php");
+ini_set("max_execution_time", "500000");
+
+// echo "<pre>";
+// print_r($_SESSION);
+// echo "</pre>";
+
+$sql = "SELECT * FROM users LIMIT 10";
+
+echo "<pre>";
+print_r($sql);
+echo "</pre>";
+
+$query = db_select($sql);
+
+
+echo "<pre>";
+//print_r($query[1]['BXCVPR']);
+echo 'Numero de registros = '.count($query);
+echo '<br>En caso de error = '.db_error();
+//print_r($query);
+echo "</pre>";
+
+echo "<pre>#|";
+foreach ($query[0] as $key2 => $value2) {
+	echo trim($key2)."|";
+}
+echo "<br>";
+$i = 1;
+foreach ($query as $key => $value) {
+
+	echo $i."|";
+	foreach ($value as $key1 => $valor) {
+		echo utf8_encode(trim($valor))."|";
+	}
+	$i++;
+	echo "<br>";
+	flush();
+	ob_flush();
+}
+
+echo "</pre>";
+
+
+?>
