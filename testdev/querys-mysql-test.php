05194025725437cdc615efcce9281bbed2591340
Carlos Segoviano <jcsegoviano@planeta.com.mx>
2024-04-19 20:50:44 +0200

    test para sql tb_usuarios

--- testdev/querys-mysql-test.php
+++ testdev/querys-mysql-test.php
@@ -6,13 +6,13 @@ echo "<pre>";
 print_r($_SESSION);
 echo "</pre>";
 
-$sql 	= "SELECT * FROM users WHERE username='marketing'";
+$sql 	= "SELECT * FROM tb_usuarios WHERE username='marketing'";
 $query 	= db_select($sql);
 
 echo "<pre>";
 
 foreach ($query as $key => $value) {
- 	echo $value['username'].'|'.$value['nivel'].'|'.$value['password'].'<br>';
+ 	echo $value['username'].'|'.$value['nivel'].'|'.$value['password'].'|'.$value['situacion'].'<br>';
 }
 
 echo "</pre>";
