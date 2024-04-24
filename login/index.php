f788a153a12e5b25047b273900a935505e693b27
Carlos Segoviano <jcsegoviano@planeta.com.mx>
2024-04-24 20:18:45 +0200

    Se agrega eliminacion de cookies usuario por etapa de desarrollo

--- login/index.php
+++ login/index.php
@@ -3,6 +3,9 @@
 include("../conexion/conexion.php");
 include("../conexion/functions.php");
 
+setcookie('tk_sesion', '', time() - 3600);
+setcookie('utk_sesion', '', time() - 3600);
+
 // Verificar si la cookie de sesión está presente y es válida
 if (isset($_COOKIE['tk_sesion'])) {
     // La cookie de sesión está presente, puedes hacer cualquier validación adicional aquí
