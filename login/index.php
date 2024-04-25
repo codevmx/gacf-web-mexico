2f69ac56087c8bbc6625c46395114592f7559e3b
Carlos Segoviano <jcsegoviano@planeta.com.mx>
2024-04-25 02:20:36 +0200

    Correccion de loggedin

--- login/index.php
+++ login/index.php
@@ -3,15 +3,21 @@
 include("../conexion/conexion.php");
 include("../conexion/functions.php");
 
-// Verificar si la cookie de sesión está presente y es válida
-if (isset($_COOKIE['tk_sesion'])) {
-    // La cookie de sesión está presente, puedes hacer cualquier validación adicional aquí
-    header("Location: gacf-usuario-pwd");
+if (!loggedin()) {
+    header("Location: gacf-dashboard");
     exit();
-} else {
-    // La cookie de sesión no está presente, redirigir al usuario a la página de inicio de sesión
-    header("Location: gacf-sesion-pwd");
-    exit();
+}else{
+    // Verificar si la cookie de sesión está presente y es válida
+    if (isset($_COOKIE['tk_sesion'])) {
+        // La cookie de sesión está presente, puedes hacer cualquier validación adicional aquí
+        header("Location: gacf-usuario-pwd");
+        exit();
+    } else {
+        // La cookie de sesión no está presente, redirigir al usuario a la página de inicio de sesión
+        header("Location: gacf-sesion-pwd");
+        exit();
+    }
 }
+
 
 ?>
